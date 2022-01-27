ace7df9606c4f37277ff2d663e400979e4a0dc4d
harry3917 <harshal.dubey@knoldus.com>
2022-01-27 16:12:04 +0530

    Classes And Objects Assignment

---
+++ src/main/scala/edu/knoldus/Zero.scala
@@ -0,0 +1,15 @@
+package edu.knoldus
+
+object Zero extends Nat {
+
+  def error(string:String)=throw new Exception(string)
+  def isZero: Boolean = true
+
+  def predecessor: Nat = error("Negative Number.")
+
+  def successor: Nat = new Succ(Zero)
+
+  def +(that: Nat): Nat = that
+
+  def -(that: Nat): Nat = if (that.isZero) Zero else error("Negative Number")
+}
